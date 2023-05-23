e60081ca025f2c0538fcb02906707cef5b8378d4
SebastianOlivera <brunos.olivera@correo.ucu.edu.uy>
2023-05-23 12:29:58 -0300

    Angular 2023

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,17 @@
+let name = "Sebastian";
+const name2: string = "Bruno";
+
+let hpPoints: number | string = 600; //Se usa el pipe para decir que se pude usar number O string
+const isAlive: Boolean = true
+
+hpPoints = 'FULL'
+
+console.log({
+    name,
+    hpPoints,
+    isAlive
+}
+
+)
+
+export {};
